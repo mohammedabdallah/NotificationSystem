2ee1f9445306aa85616387b48b657db7b84ca5fc
m.abdallah_at_areeb <mohamed.abdallah@areebgroup.com>
2020-11-14 23:50:40 +0200

    add validation on customer create

--- models/customer.js
+++ models/customer.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const customerSchema = new mongoose.Schema({
-    name: { type: String },
-    mobile: { type: String },
-    email: { type: String },
+    name: { type: String,required:true },
+    mobile: { type: String, required:true },
+    email: { type: String, required:true },
     token: { type: String },
 });
 const Customer = mongoose.model("customer", customerSchema);
